afa5c7ef5df73542e408cdbed77083250495d41c
zambarepratiksha <pratikshazende1998@gmail.com>
2023-04-13 09:27:31 -0700

    Array method

---
+++ array.js
@@ -0,0 +1,17 @@
+let myArr =["fan","camera",73,null,true];
+// Array method
+console.log(myArr.length);
+console.log(myArr);
+ myArr.pop();
+ console.log(myArr);
+myArr.push("pratiksha");
+console.log(myArr);
+// myArr.shift()
+// console.log(myArr);
+const newlen=myArr.unshift("pratik")
+console.log(newlen);
+console.log(myArr);
+myArr.toString()
+console.log(myArr);
+myArr.sort()
+console.log(myArr);
